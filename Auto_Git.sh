a98e48db0cd57033ccca3e6aea29d9ddc241542a
magicmmp <2233973404@qq.com>
2019-09-22 21:33:38 +0800

    auto git push  MDK5 project of stm32F103: 2019年09月22日 21:33:38

--- Auto_Git.sh
+++ Auto_Git.sh
@@ -2,7 +2,9 @@
 
 git add *
 
-git commit -m "auto git push  MDK5 project of stm32F103"
+date_time=$(date)
+
+git commit -m "auto git push  MDK5 project of stm32F103: ${date_time}"
 
 git push origin master
 
