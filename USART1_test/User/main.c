fdab56b6b086237e1484aa909d3bfa118ccfeb0f
magicmmp <2233973404@qq.com>
2020-02-14 19:31:19 +0800

    auto git push  MDK5 project of stm32F103: 2020年02月14日 19:31:18

--- USART1_test/User/main.c
+++ USART1_test/User/main.c
@@ -5,7 +5,7 @@
 #include "usart.h"
  
 /************************************************
- ALIENTEK精英STM32开发板实验4
+ 
  串口 实验   
  技术支持：www.openedv.com
  淘宝店铺：http://eboard.taobao.com 
@@ -13,6 +13,7 @@
  广州市星翼电子科技有限公司  
  作者：正点原子 @ALIENTEK
 ************************************************/
+
  int main(void)
  {		
  	u16 t;  
@@ -25,6 +26,7 @@
 	KEY_Init();          //初始化与按键连接的硬件接口
  	while(1)
 	{
+		delay_ms(1); 
 		if(USART_RX_STA&0x8000)
 		{					   
 			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
@@ -45,11 +47,14 @@
 				printf("\r\n精英STM32开发板 串口实验\r\n");
 				printf("正点原子@ALIENTEK\r\n\r\n");
 			}
-			if(times%1000==0)
+			if(times%5000==0)
 				printf("请输入数据,以回车键结束\r\n");  
-			if(times%50==0)
+			if(times%1000==0)
+			{
 				LED0=!LED0;//闪烁LED,提示系统正在运行.
-			delay_ms(10);   
+				LED1=!LED1;
+				LED2=!LED2;
+			}
 		}
 	}	 
  }
